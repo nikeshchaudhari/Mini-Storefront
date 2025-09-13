c8bd95cdc8bf452b30c4a7689d521fc130c4eb36
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-13 21:03:41 +0545

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,66 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className="w-full h-20 bg-[#f8f9fa] flex justify-between items-center">
         <div className="ml-5">
-          <h1 className="text-[20px] lg:text-[30px] ">Mini Store</h1>
+          <Link to="/home">
+            {" "}
+            <h1 className="text-[20px] lg:text-[30px] ">Mini Store</h1>
+          </Link>
         </div>
         <div className="hidden md:flex gap-10  ">
-          <NavLink>Home</NavLink>
-          <NavLink>Shop</NavLink>
-          <NavLink>About</NavLink>
-          <NavLink>Contact Us</NavLink>
+          <NavLink
+            to="/home"
+            className={({ isActive }) => {
+              return isActive ? "text-[#1077A3]" : "text-black";
+            }}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/shop"
+            className={({ isActive }) => {
+              return isActive ? "text-[#1077A3]" : "text-black";
+            }}
+          >
+            Shop
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => {
+              return isActive ? "text-[#1077A3]" : "text-black";
+            }}
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/contact-us"
+            className={({ isActive }) => {
+              return isActive ? "text-[#1077A3]" : "text-black";
+            }}
+          >
+            Contact Us
+          </NavLink>
         </div>
         <div className="mr-5 md:mr-20">
-            <LocalMallOutlinedIcon/>
-
+          <LocalMallOutlinedIcon />
         </div>
+      </div>
+      {/* Hamburger */}
+      <div>
+        {isOpen ? (
+          <>
+            <CloseOutlinedIcon />
+          </>
+        ) : (
+          <MenuOutlinedIcon />
+        )}
       </div>
     </>
   );
