6ec9120d4f7e8ee93f032e9c16891ffe1727dc98
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-13 20:18:04 +0545

    Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+
+    <>
+    <div className='w-full h-20 bg-[#f8f9fa] flex items-center'>
+        <div className=''>
+            <h1 className='text-[30px] mx-20'>Mini Store</h1>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Navbar
