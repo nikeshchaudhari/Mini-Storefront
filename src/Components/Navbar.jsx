055e234c2735da3966e0928b67d9a0cb5c546786
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-13 20:48:07 +0545

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
 const Navbar = () => {
   return (
-
     <>
-    <div className='w-full h-20 bg-[#f8f9fa] flex items-center'>
-        <div className=''>
-            <h1 className='text-[30px] mx-20'>Mini Store</h1>
+      <div className="w-full h-20 bg-[#f8f9fa] flex justify-between items-center">
+        <div className="ml-5">
+          <h1 className="text-[20px] lg:text-[30px] ">Mini Store</h1>
         </div>
-        <div>
-            
+        <div className="hidden md:flex gap-10  ">
+          <NavLink>Home</NavLink>
+          <NavLink>Shop</NavLink>
+          <NavLink>About</NavLink>
+          <NavLink>Contact Us</NavLink>
         </div>
-    </div>
-    
-    </>
-  )
-}
+        <div className="mr-5 md:mr-20">
+            <LocalMallOutlinedIcon/>
 
-export default Navbar
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
