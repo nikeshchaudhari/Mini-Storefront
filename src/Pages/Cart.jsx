37e260f3072cc0862d47a3f629c46ad086c7bceb
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-15 00:40:06 +0545

    Mini store

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import cartStore from "../store/store";
-
+import CloseIcon from '@mui/icons-material/Close';
 const Cart = ({ cartOpen, cartClose }) => {
   const { cart, removeCart, updateCart, clearCart } = cartStore();
 
@@ -18,7 +18,30 @@ const Cart = ({ cartOpen, cartClose }) => {
               ? "translate-x-0  md:translate-x-0 transition duration-700"
               : "translate-y-full  md:translate-y-full transition duration-1000"
           }`}
-        ></div>
+        >
+        <div className="flex justify-between px-5 mt-5">
+            <h1>Shopping Cart</h1>
+            <CloseIcon className="cursor-pointer" onClick={()=>cartClose(true)}/>
+
+        </div>
+        <hr className="mt-5" />
+       {cart.length === 0 ?(
+        <p className=" flex items-center w-full h-full justify-center text-[20px]"> Cart is empty </p>
+       ):(
+        <div>
+           {cart.map((item)=>(
+          <div key={item.id} className="flex  mt-5">
+           <div className="border w-20 h-20 flex justify-center p-2 mx-5">
+             <img src={item.image} alt="" className="w-15"/>
+           </div>
+           <div className="w-[300px] " >
+            <h1>{item.title}</h1>
+           </div>
+          </div>
+        ))}
+        </div>
+       )}
+        </div>
       
     </>
   );
