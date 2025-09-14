47c3ece9590899438b4f3dc45882135f0c8a22fa
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-14 18:54:59 +0545

    Mini store

--- src/Components/TrendProduct.jsx
+++ src/Components/TrendProduct.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const TrendProduct = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
   useEffect(() => {
     const dataFetch = async () => {
       try {
@@ -29,13 +31,13 @@ const TrendProduct = () => {
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-5 mt-10">
           {items.map((i) => (
             <div key={i.id} className=" bg-white shadow rounded p-2 ">
-              <div className=" relative flex justify-center group">
+              <div className=" relative flex justify-center group" onClick={()=>navigate(`/product-details/${i.id}`)}>
                 <img
                   src={i.image}
                   alt={i.title}
-                  className="w-[200px] p-2 cursor-pointer transition transform hover:-translate-y-2  hover:duration-1000 "
+                  className="md:w-[200px] md:h-[250px] sp-2 cursor-pointer transition transform hover:-translate-y-2  hover:duration-1000 "
                 />
-                <div className="absolute text-center bottom-0 w-full p-2 text-white bg-black hover:bg-[#1077A3]  opacity-0 group-hover:opacity-80  hover:duration-1000 cursor-pointer flex justify-center gap-2" >
+                <div className="absolute text-center bottom-0 w-full p-2 text-white bg-black hover:bg-[#1077A3]  opacity-0 group-hover:opacity-80  hover:duration-1000 cursor-pointer flex justify-center gap-2"  >
                     <h1>Add Cart</h1>
                     <span><LocalMallOutlinedIcon/></span>
                 </div>
@@ -43,7 +45,7 @@ const TrendProduct = () => {
               <div className="flex flex-col p-5">
                 <h3 className="line-clamp-1">{i.title}</h3>
                 <p>Price : {i.price}</p>
-                <p>Rating : {i.rating?.rate}</p>
+                <p>Categoty : {i.category}</p>
               </div>
             </div>
           ))}
