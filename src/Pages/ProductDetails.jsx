7a3bfc675acb674494e6eb49aad68c0ffa3a9671
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-14 17:09:07 +0545

    Mini store

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 const ProductDetails = () => {
-  return (
-    <div>ProductDetails</div>
-  )
-}
+  const { id } = useParams();
+  console.log(id[0]);
 
-export default ProductDetails
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      try {
+        const res = await axios.get(`https://fakestoreapi.com/products/${id}`);
+        setProducts(res.data);
+        console.log(res.data);
+      } catch (err) {
+        console.log("Something error", err);
+      }
+    };
+    dataFetch();
+  }, [id]);
+  if (!products) {
+    return (
+      <div>
+        <h1>Loading....</h1>
+      </div>
+    );
+  }
+  return (
+    <>
+      <Navbar />
+      <div className="md:flex md:mx-10 mt-10 overflow-hidden ">
+        <div className="md:shadow-2xl w-full md:w-[500px] flex justify-center p-2">
+          <img src={products.image} alt="" className=" w-[200px] md:w-[300px] md:h-[400px]" />
+        </div>
+        <div className="mx-10 md:w-[500px]">
+          <h1 className="md:text-[20px] text-[#1077A3] mb-3">
+            {" "}
+            {products.category}
+          </h1>
+
+          <h3 className="text-[23px] font-bold line-clamp-2 mb-3">
+            {products.title}
+          </h3>
+          <p className="text-[25px] font-bold">Price : {products.price}</p>
+          <p>{products.description}</p>
+          <div className="flex justify-between   p-2 w-70 mt-2 cursor-pointer items-center">
+            <div className="flex gap-5 justify-between items-center shadow p-2">
+              <div className="hover:bg-black/5 p-1 rounded">
+                <AddIcon />
+              </div>
+              <span>hii</span>
+              <div className="hover:bg-black/5 p-1 rounded">
+                <RemoveIcon />
+              </div>
+            </div>
+            <div>
+              <button className="bg-red-700 p-2 text-white cursor-pointer hover:bg-red-900 transition hover:duration-700">
+                <h4>Add to Cart</h4>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductDetails;
