85e9a3e5498bd6e2d74b4dc33b2742bd91bae3bf
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-14 09:23:14 +0545

    Trend

--- src/Components/TrendProduct.jsx
+++ src/Components/TrendProduct.jsx
@@ -1,42 +1,52 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
 
 const TrendProduct = () => {
   const [items, setItems] = useState([]);
-  const [loading,setLoading]= useState(true)
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const dataFetch = async () => {
       try {
         const res = await axios.get("https://fakestoreapi.com/products");
-        setItems(res.data.slice(0,4));
-        setLoading(false)
-        console.log(res.data.slice(0,4));
+        setItems(res.data.slice(0, 4));
+        setLoading(false);
+        console.log(res.data.slice(0, 4));
       } catch (err) {
-        console.log("Product Not",err);
-        setLoading(true)
-        
+        console.log("Product Not", err);
+        setLoading(true);
       }
     };
     dataFetch();
-  },[]);
+  }, []);
   return (
     <div className="overflow-hidden">
       <>
-        <h1 className=" text-center mt-5 text-[20px] md:text-[30px] font-bold">Trending Products</h1>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-5">
-            {items.map((i)=>(
-                <div key={i.id} className=" bg-white shadow rounded p-2 ">
-                   
-                   <div className="flex justify-center">
-                     <img src={i.image} alt={i.title} className="w-[200px] p-2 cursor-pointer transition transform hover:-translate-y-2  hover:duration-700"/>
-                   </div>
-                   <div className="flex flex-col p-5">
-                    <h3 className="line-clamp-1">{i.title}</h3>
-                    <p>Price : {i.price}</p>
-                    <p>Rating : {i.rating?.rate}</p>
-                   </div>
+        <h1 className=" text-center mt-10 text-[20px] md:text-[30px] font-bold">
+          Trending Products
+        </h1>
+
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-5 mt-10">
+          {items.map((i) => (
+            <div key={i.id} className=" bg-white shadow rounded p-2 ">
+              <div className=" relative flex justify-center group">
+                <img
+                  src={i.image}
+                  alt={i.title}
+                  className="w-[200px] p-2 cursor-pointer transition transform hover:-translate-y-2  hover:duration-1000 "
+                />
+                <div className="absolute text-center bottom-0 w-full p-2 text-white bg-black hover:bg-[#1077A3]  opacity-0 group-hover:opacity-80  hover:duration-1000 cursor-pointer flex justify-center gap-2" >
+                    <h1>Add Cart</h1>
+                    <span><LocalMallOutlinedIcon/></span>
                 </div>
-            ))}
+              </div>
+              <div className="flex flex-col p-5">
+                <h3 className="line-clamp-1">{i.title}</h3>
+                <p>Price : {i.price}</p>
+                <p>Rating : {i.rating?.rate}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </>
     </div>
