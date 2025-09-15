104825c4b877f99d13000e90f9eb9a84e0d52e3e
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-15 14:15:52 +0545

    Mini store

--- src/Components/TrendProduct.jsx
+++ src/Components/TrendProduct.jsx
@@ -2,11 +2,16 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
 import { Navigate, useNavigate } from "react-router-dom";
+import cartStore from "../store/store";
+import { toast } from "react-toastify";
 
 const TrendProduct = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+  const cart = cartStore((state) => state.cart);
+  const addCart = cartStore((state) => state.addCart);
+
   useEffect(() => {
     const dataFetch = async () => {
       try {
@@ -21,6 +26,17 @@ const TrendProduct = () => {
     };
     dataFetch();
   }, []);
+
+  const handleAddCart = (products) => {
+    
+    const alreadyAdd = cart.find((f) => f.id === products.id);
+    if (alreadyAdd) {
+      toast.error("Already Add Products..");
+    } else {
+      toast.success("Add to cart");
+      addCart({ ...products, quantity: 1 });
+    }
+  };
   return (
     <div className="overflow-hidden">
       <>
@@ -31,15 +47,26 @@ const TrendProduct = () => {
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-5 mt-10">
           {items.map((i) => (
             <div key={i.id} className=" bg-white shadow rounded p-2 ">
-              <div className=" relative flex justify-center group" onClick={()=>navigate(`/product-details/${i.id}`)}>
+              <div
+                className=" relative flex justify-center group"
+                onClick={() => navigate(`/product-details/${i.id}`)}
+              >
                 <img
                   src={i.image}
                   alt={i.title}
-                  className="md:w-[200px] md:h-[250px] sp-2 cursor-pointer transition transform hover:-translate-y-2  hover:duration-1000 "
+                  className="w-[150px] md:w-[200px] h-[200px] md:h-[250px] sp-2 cursor-pointer transition transform hover:-translate-y-2  hover:duration-1000 "
                 />
-                <div className="absolute text-center bottom-0 w-full p-2 text-white bg-black hover:bg-[#1077A3]  opacity-0 group-hover:opacity-80  hover:duration-1000 cursor-pointer flex justify-center gap-2"  >
-                    <h1>Add Cart</h1>
-                    <span><LocalMallOutlinedIcon/></span>
+                <div
+                  className="absolute text-center bottom-0 w-full  p-2 text-white bg-black hover:bg-[#1077A3] opacity-100 lg:opacity-0 lg:group-hover:opacity-80  hover:duration-1000 cursor-pointer flex justify-center gap-2"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleAddCart(i);
+                  }}
+                >
+                  <h1>Addcard</h1>
+                  <span>
+                    <LocalMallOutlinedIcon />
+                  </span>
                 </div>
               </div>
               <div className="flex flex-col p-5">
